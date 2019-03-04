d7ff966897fd66abeb45648fa7fc72cce4e1e88c
Jonathan <jonathanmatheus1997.jm@gmail.com>
2019-03-04 16:54:53 -0300

    [#AppBarber] Criando listagem de provedores de serviços.

--- App Barber/src/routes.js
+++ App Barber/src/routes.js
@@ -8,6 +8,7 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucess = req.flash('sucess')
@@ -25,9 +26,6 @@ routes.use('/app', authMiddleware)
 
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  // console.log(req.session)
-  res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
